039ac33b7fbcf0ead6f2a49745de14a075a4f25d
Raúl Osuna <rosuna@suse.com>
2023-12-08 21:35:18 +0100

    Refactor frontend using Bootstrap 5

--- main.py
+++ main.py
@@ -4,21 +4,34 @@ from post import Post
 
 app = Flask(__name__)
 
-blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+blog_url = "https://api.npoint.io/79af83afe03463a29f67"
 response = requests.get(blog_url)
 all_posts = response.json()
 post_objects = [Post(post["id"],
                      post["title"],
                      post["subtitle"],
                      post["body"],
+                     post["date"],
+                     post["author"],
+                     post["image"],
                      ) for post in all_posts]
 
 @app.route('/')
 def home():
     return render_template("index.html", posts=post_objects)
+    #return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 @app.route('/post/<int:index>')
 def show_post(index):
+    requested_post = None
     for post in post_objects:
         if post.id == index:
             requested_post = post
--- post.py
+++ post.py
@@ -1,6 +1,9 @@
 class Post:
-    def __init__(self, post_id, title, subtitle, body):
+    def __init__(self, post_id, title, subtitle, body, date, author, image):
         self.id = post_id
         self.title = title
         self.subtitle = subtitle
         self.body = body
+        self.date = date
+        self.author = author
+        self.image = image
